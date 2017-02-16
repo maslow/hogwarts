459936ed1dc1724289e697318715da582f18495c
maslow <wangfugen@126.com>
2017-02-16 16:41:06 +0800

    RM sample_data/jobs;

--- sample_data/jobs/4/src/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-http.get(process.argv[2], response => {
-    let buffers = []
-    response.on('data', data => buffers.push(data.toString()))
-    response.on('end', () => {
-        console.log(buffers.join(''))
-    })
-})
